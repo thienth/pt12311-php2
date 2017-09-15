9e0592aa7d660188cb210065b4d205e43633c1e6
Trần Hữu Thiện <thienth32@gmail.com>
2017-09-15 13:35:27 +0700

    update

---
+++ lesson2/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * LAB1 - tạo cơ sở dữ liệu lab1 gồm bảng 
+ * products
+ * id - int - pk -ai
+ * name - varchar
+ * price - decimal(10, 2)
+ * detail - text
+ *
+ * thêm ít nhất 10 bản ghi vào bảng produts
+ *
+ * 
+ * Sử dụng lớp BaseModel để tạo ra 1 biến chứa kết nối đến csdl
+ * sau đó thực hiện tương tác với csql để lấy ra danh sách sản phẩm và hiển thị dưới dạng bảng tại file index.php
+ */
+
+
+
+
+ ?>
---
+++ lesson2/models/BaseModel.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+* 
+*/
+class BaseModel
+{
+	public $connect;
+
+	// Khi new Base model o dau thi lap tuc doi tuong moi se co ket noi den csdl luon
+	function __construct()
+	{
+		# code...
+	}
+}
+
+
+ ?>
